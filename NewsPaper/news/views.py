0efc1c0f384a5043665811e0608f791f44bed8e8
sobago <isobago@gmail.com>
2022-04-12 11:51:57 +0300

    D3.3, Настроен вывод списка статей и отображение страницы со статьей, добавлены ссылки для перехода к статьям

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
+from pprint import pprint
 
 
 # Create your views here.
@@ -10,9 +12,14 @@ def index(request):
 
 class PostsList(ListView):
     model = Post
-    ordering = 'post_text'
+    ordering = '-create_date_time'
     template_name = 'news.html'
     context_object_name = 'posts'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     pprint(context)
+    #     return context
 
 
 class PostDetail(DetailView):
