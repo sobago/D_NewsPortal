e7a0fc1783cb611a9e7b9626a7ef956f262577c1
Evgenii Novik <isobago@gmail.com>
2022-06-01 23:30:45 +0300

    D11.4 Добавлена команда удаления постов из категории

---
+++ NewsPaper/news/management/commands/deleteposts.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...models import Post, Category
+
+
+class Command(BaseCommand):
+    help = 'Команда deleteposts удаляет все посты из выбранной категории.'
+    requires_migrations_checks = True
+
+    def handle(self, *args, **options):
+        self.stdout.readable()
+        self.stdout.write('Введите название категории, из которой нужно удалить все посты')
+        cats = [x.cat_name for x in Category.objects.all()]
+        self.stdout.write(f'Имеющиеся категории: {cats}')
+        answer = input('Категория: ')
+        Category.objects.get(cat_name=answer)
+        Post.objects.filter(post_to_category_rel__cat_name=answer).delete()
+        self.stdout.write(f'Все посты из категории {answer} удалены.')
+        return
