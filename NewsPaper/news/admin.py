7a2564108108e6e50529a0166f66f8781346b253
Evgenii Novik <isobago@gmail.com>
2022-06-02 20:12:30 +0300

    D11.5 Отредактирование отображение в админке постов и комментариев

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Author, Category, Post, Comment
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'create_date_time', 'post_title', 'post_to_category_rel', 'author', 'choice_type')
+    list_filter = ('post_to_category_rel', 'author', 'choice_type')
+    search_fields = ('post_title', 'post_text')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'create_date_time', 'comment_text', )
+    list_filter = ('create_date_time', )
+    search_fields = ('comment_text', )
+
+
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
